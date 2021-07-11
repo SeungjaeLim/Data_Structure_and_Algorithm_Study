5f545bdb34f853d45bf6c9a56f0770b9c81fd0e3
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-07-11 15:10:25 +0900

    DAY1-Performance Analysis by linear, binary search

---
+++ DAY1_Performance_Analysis/Linear_Search/데이터 탐색/cu3001.c
@@ -0,0 +1,28 @@
+﻿#include<stdio.h>
+int n[100001];
+int lsearch(int request, int len);
+int main()
+{
+    int len,query;
+    scanf("%d",&len);
+    for(int i = 0; i<len ;i++)
+    {
+        scanf("%d", &n[i]);
+    }
+    scanf("%d", &query);
+    printf("%d", lsearch(query, len));
+    return 0;
+}
+int lsearch(int request, int len)
+{
+    for(int i = 0 ; i < len; i++)
+    {
+        if(n[i] == request)
+        {
+            return i+1;
+        }
+    }
+    return -1;
+}
+
+
